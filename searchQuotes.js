f271d4f305701858e14ada4c8bb75106a2030a03
altcode255 <anonte3p5usu@gmail.com>
2024-08-24 02:36:45 -0700

    Improve filter

--- searchQuotes.js
+++ searchQuotes.js
@@ -1,16 +1,16 @@
-
-
 let html;
 var items = document.getElementsByClassName("page-link");
 for (let i = 0; i < items.length; i++) {
-    items.item(i).addEventListener("click", fetchText, false);
-  }
-
+  items.item(i).addEventListener("click", fetchText, false);
+}
 
 function fetchText() {
   var converter = new showdown.Converter();
   converter.setOption("simpleLineBreaks", true);
-  var url = "https://raw.githubusercontent.com/ALTCODE255/namelessquotebots/main/tweets/" + this.innerHTML.substring(1) + ".txt";
+  var url =
+    "https://raw.githubusercontent.com/ALTCODE255/namelessquotebots/main/tweets/" +
+    this.innerHTML.substring(1) +
+    ".txt";
   fetch(url)
     .then((response) => response.text())
     .then((text) =>
@@ -23,11 +23,16 @@ function fetchText() {
       html = converter.makeHtml(cleanText);
       document.getElementById("quotes").innerHTML = html;
     });
+  document.getElementById("search").disabled = false;
 }
 
 function filterText() {
   var re = new RegExp(String.raw`<li>(?!.*${this.value}).*</li>`, "gmi");
-  document.getElementById("quotes").innerHTML = html.replaceAll(re, "");
+  document.getElementById("quotes").innerHTML = html
+    .replaceAll(re, "")
+    .replaceAll(/<ul>\s*<\/ul>/gm, "")
+    .replaceAll(/<h3 id=.*>.*<\/h3>(?!\s*<ul>)/gm, "")
+    .replaceAll(/<h2 id=.*>.*<\/h2>(?=(\s*<h2))/gm, "");
 }
 
 el = document.getElementById("search");
