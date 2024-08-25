a505114a43d5a7c408327e627976ae56556ba8f9
altcode255 <anonte3p5usu@gmail.com>
2024-08-25 01:35:56 -0700

    Fix for mobile Firefox

--- searchQuotes.js
+++ searchQuotes.js
@@ -1,67 +1,69 @@
 let html;
 
-addEventListener("DOMContentLoaded", fetchText, false);
-window.addEventListener("hashchange", (event) => {
-  fetchText();
-});
-
-function fetchText() {
-  if (window.location.hash) {
-    let converter = new showdown.Converter();
-    converter.setOption("simpleLineBreaks", true);
-    let url =
-      "https://gist.githubusercontent.com/ALTCODE255/947280561356cf037ce6f749638277d5/raw/" +
-      window.location.hash.substring(1) +
-      ".txt";
-    fetch(url)
-      .then((response) => {
-        if (response.status == 200) {
-          return response.text();
-        } else {
-          throw new Error("Status code " + response.status);
-        }
-      })
-      .then((text) => {
-        html = converter
-          .makeHtml(text)
-          .replaceAll(/<em>|<\/em>/g, "*")
-          .replaceAll("\\n", "<br>");
-        document.getElementById("quotes").innerHTML = html;
-        document.getElementById("search").disabled = false;
-        countQuotes();
-        setActiveSection();
-      });
+function loadQuotes() {
+  let quoteSets = document.getElementById("quoteSets").children;
+  for (let set of quoteSets) {
+    let url = `https://gist.githubusercontent.com/ALTCODE255/947280561356cf037ce6f749638277d5/raw/${set.id}.txt`;
+    fetchQuotes(url, set);
   }
-  return false;
+  document
+    .getElementById("search")
+    .addEventListener("input", filterText, false);
 }
 
-function setActiveSection() {
-  document.querySelector(
-    `.page-link[href='${window.location.hash}']`
-  ).className = "page-link active";
-  var anchors = document.querySelectorAll(
-    `.page-link:not([href='${window.location.hash}'])`
-  );
-  for (var a of anchors) {
-    a.className = "page-link";
-  }
+function fetchQuotes(url, el) {
+  let converter = new showdown.Converter();
+  converter.setOption("simpleLineBreaks", true);
+  fetch(url)
+    .then((response) => {
+      if (response.status == 200) {
+        return response.text();
+      } else {
+        throw new Error("Status code " + response.status);
+      }
+    })
+    .then((text) => {
+      el.innerHTML = converter
+        .makeHtml(text)
+        .replaceAll(/<em>|<\/em>/g, "*")
+        .replaceAll("\\n", "<br>");
+      if (window.location.hash.substring(1) == el.id) {
+        updateActiveSection(el.id);
+        document.getElementById(el.id + "-radio").checked = "true";
+      }
+    });
 }
 
-function countQuotes() {
+function updateActiveSection(id) {
+  activeSection = document.getElementById(id);
+
+  // disable other sections
+  let sections = document.querySelectorAll(`#quoteSets > div:not(#${id})`);
+  for (let s of sections) {
+    if (s.className == "active") {
+      s.innerHTML = html;
+    }
+    s.removeAttribute("class");
+  }
+
+  // activate section
+  activeSection.className = "active";
+  html = activeSection.innerHTML;
+  countQuotes(activeSection);
+}
+
+function countQuotes(el) {
   document.getElementById("results").innerHTML =
-    "Results: " +
-    document.getElementById("quotes").querySelectorAll("li").length +
-    " quote(s)";
+    "Results: " + el.querySelectorAll("li").length + " quote(s)";
 }
 
 function filterText() {
-  var nonmatch = new RegExp(String.raw`<li>(?!.*${this.value}).*</li>`, "gmi");
-  document.getElementById("quotes").innerHTML = html
+  let activeSection = document.querySelector("#quoteSets > div.active");
+  let nonmatch = new RegExp(String.raw`<li>(?!.*${this.value}).*</li>`, "gmi");
+  activeSection.innerHTML = html
     .replaceAll(nonmatch, "")
     .replaceAll(/<ul>\s*<\/ul>/gm, "")
     .replaceAll(/<h3.*h3>(?![\s]*<ul>)/gm, "")
     .replaceAll(/<h2.*h2>(?![\s]*(<ul>|<h3.*h3>))/gm, "");
-  countQuotes();
+  countQuotes(activeSection);
 }
-
-document.getElementById("search").addEventListener("input", filterText, false);
