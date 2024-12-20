fb7d1b80815a7fb38b5c50359ea3d8ba708d7a98
altcode255 <anonte3p5usu@gmail.com>
2024-12-20 02:30:20 -0800

    add plot

--- scripts/liveTable.js
+++ scripts/liveTable.js
@@ -9,6 +9,11 @@ function getLiveTable() {
     )
         .then((res) => res.json())
         .then((data) => {
+            let x_val = new Array();
+            let y_akc = new Array();
+            let y_sth = new Array();
+            let y_sp = new Array();
+
             table = `
                 <table class="table table-bordered table-striped">
                     <thead>
@@ -21,15 +26,75 @@ function getLiveTable() {
                     </thead>
                 `;
             for (let idx = 0; idx <= 30; idx++) {
-                value = Object.values(data)[idx];
+                item = Object.values(data)[idx];
                 table += `
                     <tr>
-                        <td class="px-3">${value.date}</td>
-                        <td class="px-3">${value.akechi}</td>
-                        <td class="px-3">${value.sonic}</td>
-                        <td class="px-3">${value.shuuen}</td>
+                        <td class="px-3">${item.date}</td>
+                        <td class="px-3">${item.akechi}</td>
+                        <td class="px-3">${item.sonic}</td>
+                        <td class="px-3">${item.shuuen}</td>
                     </tr>`;
+                x_val.push(item.date);
+                y_akc.push(item.akechi);
+                y_sth.push(item.sonic);
+                y_sp.push(item.shuuen);
             }
+
+            graph = document.getElementById("graph");
+            layout = {
+                title: "Word Count Over Past 30 Days",
+                paper_bgcolor: "rgba(0, 0, 0, 0)",
+                plot_bgcolor: "rgba(0, 0, 0, 0)",
+                font: {
+                    color: 'white',
+                },
+                xaxis: {
+                    fixedrange: true,
+                    showgrid: false,
+                },
+                yaxis: {
+                    fixedrange: true,
+                    showgrid: false,
+                },
+            };
+            plot_data = [
+                {
+                    x: x_val,
+                    y: y_akc,
+                    mode: "lines",
+                    name: "Akechi",
+                    marker: {
+                        color: "rgb(216, 59, 59)"
+                    }
+                },
+                {
+                    x: x_val,
+                    y: y_sth,
+                    mode: "lines",
+                    name: "Sonic",
+                    marker: {
+                        color: "rgb(22, 99, 162)"
+                    }
+                },
+                {
+                    x: x_val,
+                    y: y_sp,
+                    mode: "lines",
+                    name: "Shuuen",
+                    marker: {
+                        color: "rgb(214, 51, 132)"
+                    }
+                },
+            ];
+            config = {
+                displayModeBar: false,
+            };
+            Plotly.newPlot(
+                graph,
+                plot_data,
+                layout,
+                config
+            );
 
             document.getElementById("s-table").innerHTML = table;
         });
