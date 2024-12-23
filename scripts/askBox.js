b5394486d2ac80b3417505b218c54ce3fd563482
altcode255 <anonte3p5usu@gmail.com>
2024-12-23 14:23:16 -0800

    Merge js files

--- scripts/askBox.js
+++ scripts/askBox.js
@@ -24,3 +24,24 @@ document.getElementById("ask-box").addEventListener("submit", function (event) {
     i.value = "";
   }
 });
+
+var el;
+
+function countCharacters() {
+  var textEntered, countRemaining, counter;
+  textEntered = this.value;
+  counter = textEntered.length + "/4000";
+  countRemaining = document.getElementById("charactersRemaining");
+  countRemaining.textContent = counter;
+}
+
+function autoGrow() {
+  var fontSize = parseInt(window.getComputedStyle(this).fontSize);
+  this.style.height = fontSize * 7 + "px";
+  this.style.height = this.scrollHeight + "px";
+}
+
+el = document.getElementById("textinput");
+el.addEventListener("keyup", countCharacters, false);
+el.addEventListener("input", autoGrow, false);
+
