d0c2bdc964ca95f90132dcf795fc0819af42886c
altcode255 <anonte3p5usu@gmail.com>
2025-05-30 19:59:34 -0700

    deduplicate footer and header more

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,11 +1,11 @@
-document.getElementById("footer").innerHTML = `
-    <hr>
-    <b>Page Last Updated: </b><span id="last-updated"></span>
-    <img class="d-none" id="hits" src="">
-    <hr>
-`
-
-function getLastUpdateTimestamp(id, filepath) {
+function getLastUpdateTimestamp() {
+    if (location.pathname.endsWith("/")) {
+        filepath = location.pathname.substring(1) + "index.html";
+    } else {
+        filepath = location.pathname.endsWith(".html")
+            ? location.pathname.substring(1)
+            : location.pathname.substring(1) + ".html";
+    }
     fetch(
         `https://api.github.com/repos/ALTCODE255/altcode255.github.io/commits?path=${filepath}`
     )
@@ -14,12 +14,30 @@ function getLastUpdateTimestamp(id, filepath) {
             date = new Date(res[0].commit.committer.date);
             formatDate = date.toLocaleString("sv-SE").split(" ")[0];
             formatTime = date.toLocaleTimeString();
-            document.getElementById(id).innerHTML = formatDate + " @ " + formatTime;
+            document.getElementById("last-updated").innerHTML =
+                formatDate + " @ " + formatTime;
         });
 }
 
-function getHits(path) {
-    path = path.replace("/", "%2F");
-    document.getElementById("hits").src = `https://hitscounter.dev/api/hit?url=altcode255.github.io${path}&label=Page+Visits&color=%23f5b52c`;
-    document.getElementById("hits").classList.remove("d-none");
+function getHits() {
+    path =
+        location.pathname == "/" ? "" : location.pathname.replace("/", "%2F");
+    document.getElementById(
+        "hits"
+    ).src = `https://hitscounter.dev/api/hit?url=altcode255.github.io${path}&label=Page+Visits&color=%23f5b52c`;
+    document.getElementById("unhide").classList.remove("d-none");
+}
+
+function setFooter() {
+    document.getElementById("footer").innerHTML = `
+        <hr>
+        <span id="unhide" class="d-none"><img id="hits" src=""> | </span><b>Page Last Updated: </b><span id="last-updated"></span>
+        <hr>
+    `;
+}
+
+setFooter();
+if (location.hostname == "altcode255.github.io") {
+    getLastUpdateTimestamp();
+    getHits();
 }
--- scripts/header.js
+++ scripts/header.js
@@ -1,42 +1,55 @@
-document.getElementById("header").innerHTML = `
-    <nav class="navbar navbar-expand-md navbar-dark">
-        <div class="container-fluid">
-            <ul class="navbar-nav justify-content-center w-100">
-                <li class="nav-item">
-                    <a class="nav-link" href="./">Home</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Projects
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="coding">Coding Projects</a></li>
-                        <li><a class="dropdown-item" href="bots">Twitter/Bluesky Bots</a></li>
-                        <li><a class="dropdown-item" href="counter">S Tally Counter</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="writing">Writing</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Shuuenpro Stuff
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="shuuenpro">Directory</a></li>
-                        <li><a class="dropdown-item" href="https://shuuenpro.github.io">Nameless Shuuen no Shiori Guide</a></li>
-                        <li><a class="dropdown-item" href="weekofdemise">Week of Demise (Art Week)</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="akc">Akechi Archive</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+function setHeader() {
+    document.getElementById("header").innerHTML = `
+        <nav class="navbar navbar-expand-md navbar-dark">
+            <div class="container-fluid">
+                <ul class="navbar-nav justify-content-center w-100">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Home</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Projects
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="/coding">Coding Projects</a></li>
+                            <li><a class="dropdown-item" href="/bots">Twitter/Bluesky Bots</a></li>
+                            <li><a class="dropdown-item" href="/counter">S Tally Counter</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/writing">Writing</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Shuuenpro Stuff
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="/shuuenpro">Directory</a></li>
+                            <li><a class="dropdown-item" href="https://shuuenpro.github.io">Nameless Shuuen no Shiori Guide</a></li>
+                            <li><a class="dropdown-item" href="/weekofdemise">Week of Demise (Art Week)</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="akc">Akechi Archive</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     `;
+}
 
-function setActive(href) {
+function setActive() {
+    if (location.host == "altcode255.github.io") {
+        href = location.pathname.replace(".html", "");
+    } else {
+        href = "/" + location.href.split("/").slice(-1)[0].replace(".html", "");
+        if (href == "/index") {
+            href = "/";
+        }
+    }
     let match = document.querySelectorAll(`header a[href="${href}"`)[0];
     match.classList.add("active");
 }
+
+setHeader();
+setActive();
